087004052c684f0e22e7e0cfa4e867d01e510ad9
Jérôme Kunegis <kunegis@gmail.com>
2016-05-04 15:22:10 +0000

    correct step plot (inter)

---
+++ jl/step_full.jl
@@ -0,0 +1,26 @@
+#
+# Draw a "full" step plot.
+#
+# PARAMETERS
+#
+#	x	(n) Values
+#
+
+function step_full(x)
+    
+    n = length(x);
+    
+    x_sorted = sort(x);
+
+    step(x_sorted,
+         (n:-1:1) / n,
+         linestyle="-");
+
+    # If the Y axis was not logarithmic, we show also add the point
+    # (x_sorted[end], 0) to the plot, making x_sorted[end] be there
+    # twice. 
+    
+    xscale("log");
+    yscale("log");
+
+end
