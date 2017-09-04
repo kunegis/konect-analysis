3c8eb211ce31806591b840abc20d3441c59ffbc8
Jérôme KUNEGIS <kunegis@gmail.com>
2017-09-04 14:51:45 +0200

    determine which networks have a rating scale

--- m/means_best.m
+++ m/means_best.m
@@ -1,18 +1,17 @@
 %
-% This is the additive normalization used.  This function also knows which type of networks need normalization or not. 
+% This is the additive normalization used.  This function also knows
+% which type of networks need normalization or not.   
 %
 
 function [U V] = means_best(T, m, n, weights)
 
-consts = konect_consts(); 
+%%consts = konect_consts(); 
+[negative interval_scale] = konect_data_weights(); 
 
-if weights == consts.SIGNED | weights == consts.WEIGHTED
-
+if interval_scale(weights)
+%%if weights == consts.SIGNED | weights == consts.WEIGHTED | weights.
     [U V] = means_euv(T, m, n);
-
 else
-
     U = []; 
     V = []; 
-
 end
